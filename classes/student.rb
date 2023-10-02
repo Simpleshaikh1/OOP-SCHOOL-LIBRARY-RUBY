762956f46f41ee002ded9c466f87e9cfaca9843b
Lord Zodd <hamzatbolaji648@yahoo.com>
2023-10-02 23:51:22 +0100

    RUBOCOP-RECTIFIED

--- classes/student.rb
+++ classes/student.rb
@@ -1,7 +1,9 @@
+require_relative 'person'
+
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(id, classroom, age = 18, name = 'Unknown', parent_permission: true,)
+  def initialize(id, classroom, age = 18, name = 'Unknown', parent_permission: true)
     super(id, age, name, parent_permission)
     @classroom = classroom
   end
