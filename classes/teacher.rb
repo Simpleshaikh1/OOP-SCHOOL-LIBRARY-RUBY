da12e6e7790420f8d5c269b9d22b0d3f927586ec
Lord Zodd <hamzatbolaji648@yahoo.com>
2023-10-02 23:52:54 +0100

    ALL-CLASSES-ADDED

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(id, age = 18, name = 'Unknown', parent_permission: true, specialization)
+  def initialize(id, specialization, age = 18, name = 'Unknown', parent_permission: true)
     super(id, age, name, parent_permission)
     @specialization = specialization
   end
