438251a2435b3bbea1295a4020336db33c4881f8
Lord Zodd <hamzatbolaji648@yahoo.com>
2023-10-04 10:24:08 +0100

    ASSOCIATIONS

--- classes/student.rb
+++ classes/student.rb
@@ -5,10 +5,10 @@ class Student < Person
 
   def initialize(id, classroom, age = 18, name = 'Unknown', parent_permission: true)
     super(id, age, name, parent_permission)
-    @classroom = classroom
   end
 
   def classroom=(classroom)
+    @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
 
